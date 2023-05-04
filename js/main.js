317980c4ec5140553f2d41bff4edd03a58691bc7
Sam Hanson <samhanson@Sams-MacBook-Pro.local>
2023-05-03 21:55:51 -0500

    updating vulns page

--- js/main.js
+++
@@ -1,8 +0,0 @@
-
-function loadFile() {
-  fetch('http://localhost/static-binary-analysis-guide-1/code/foo.txt')
-  .then(response => response.text())
-  .then((data) => {
-    console.log(data)
-  })
-}
